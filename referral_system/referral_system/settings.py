5d83e88270663b182074f2a5fb01f481b29d50d7
Ankur Pathak <arpathak10@gmail.com>
2024-04-07 17:54:08 +0530

    Add Mysql Database Credentials in settings file

--- referral_system/referral_system/settings.py
+++ referral_system/referral_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'user',
 ]
 
@@ -76,8 +78,15 @@ WSGI_APPLICATION = 'referral_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'referral_db',
+        'USER': 'ankur',
+        'PASSWORD': 'ankur',
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
