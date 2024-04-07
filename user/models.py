22cd5b52ac836059817c1cae2fd58d345ebcba02
Ankur Pathak <arpathak10@gmail.com>
2024-04-07 15:20:51 +0530

    User Model Created

---
+++ user/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = None
+    name = models.CharField(max_length=30, blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    password = models.TextField(blank=False, null=False)
+    refer_code = models.CharField(max_length=30, blank=False, null=False)
+    referral_points = models.IntegerField(default=0, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['refer_code'], name='unique_fields')
+        ]
+
